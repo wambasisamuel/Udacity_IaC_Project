e00c4fbb236ab87c30c836f00158848b1b8b574a
wambasisamuel <wambasisamuel@gmail.com>
2023-01-07 23:47:42 +0300

    Create deploy_network.sh

---
+++ scripts/deploy_network.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name network --template-body file://network.yml  --parameters file://network-params.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
